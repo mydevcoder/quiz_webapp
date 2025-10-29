a69ac18f5232129b43f7ad6fee6258a3c15d4c48
mydevcoder <230565081+mydevcoder@users.noreply.github.com>
2025-10-29 23:04:17 +0530

    url
    url

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Set up axios default
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your backend URL
+  baseURL: 'https://quiz-webapp-zl3m.onrender.com/api', // Your backend URL
 });
 
 const AuthContext = createContext();
