86fa8efc8718ffb2c074a657adb0f0a78e1f520c
mydevcoder <230565081+mydevcoder@users.noreply.github.com>
2025-10-29 22:57:47 +0530

    CORS
    CORS

--- backend/index.js
+++ backend/index.js
@@ -21,5 +21,14 @@ app.use('/api/quiz', require('./routes/quiz'));
 app.use('/api/admin', require('./routes/admin'));
 
 
+// Middlewares
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // Only allow your frontend
+};
+app.use(cors(corsOptions)); // Use the configured options
+app.use(express.json());
+
+// ...
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port https://localhost:${PORT}`));
